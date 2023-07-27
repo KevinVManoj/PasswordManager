e43c713f23008ac723c10d0c61f74ee33e7bf7e6
PiloTheSilo <kmanoj@uncc.edu>
2023-07-26 20:49:38 -0400

    almost complete, just need to work on encryption

--- Main.py
+++ Main.py
@@ -2,40 +2,57 @@ import string
 
 from PasswordCreator import PasswordCreator
 from PasswordReader import PasswordReader
-filePasswords = PasswordReader()
 
+filePasswords = PasswordReader()
 
 print("Welcome to Kevin's password manager,")
 print("You can either create a new password for a service you use with your own requirements")
 print("Or you can access your passwords")
 
-print("1. Create a new password")
-print("2. Access passwords")
+programUpkeep = True
+while programUpkeep == True:
+    print("1. Create a new password")
+    print("2. Add password")
+    print("3. Access passwords")
+    print("4. Exit")
 
-x = int(input())
-if x == 1:
-    while x == 1:
-        userUppercase = int(input("How many uppercase letters would you like? "))
-        userLowercase = int(input("How many lowercase letters would you like? "))
-        userNumbers = int(input("How many numbers would you like? "))
-        userSpecial = int(input("How many special characters would you like? "))
+    x = int(input())
+    if x == 1:
+        while x == 1:
+                userUppercase = int(input("How many uppercase letters would you like? "))
+                userLowercase = int(input("How many lowercase letters would you like? "))
+                userNumbers = int(input("How many numbers would you like? "))
+                userSpecial = int(input("How many special characters would you like? "))
 
-        password_generator = PasswordCreator(userUppercase, userLowercase, userNumbers, userSpecial)
-        password = password_generator.generatePassword(userUppercase, userLowercase, userNumbers, userSpecial)
-        
-        print("Here is your password:", password)
-        print("If you like your password, type 1 to assign the password. If you wish for another password, type 2 to generate another.")
-        y = int(input())
-        if y == 1:
-            x += 1
-            passwordLabeling = input("What would you like to label this password as? ")
-            userPasswordUnit = passwordLabeling + ":" + password
-
-            filePasswords.passwordEntries.append(userPasswordUnit)
-
-            print(filePasswords.passwordEntries)
-
-    
-        elif y == 2:
-            x == 1
-
+                password_generator = PasswordCreator(userUppercase, userLowercase, userNumbers, userSpecial)
+                password = password_generator.generatePassword(userUppercase, userLowercase, userNumbers, userSpecial)
+                
+                print("Here is your password:", password)
+                print("If you like your password, type 1 to assign the password. If you wish for another password, type 2 to generate another.")
+                y = int(input())
+                if y == 1:
+                    x += 1
+                    passwordLabeling = input("What would you like to label this password as? ")
+                    userPasswordUnit = passwordLabeling + ":" + password
+                    filePasswords.passwordEntries.append(userPasswordUnit)
+                    print("Password has been stored")
+                    
+                elif y == 2:
+                    x == 1
+    elif x == 2:
+        userPasswordName = input("Input the name of the password: ")
+        userPasswordPassword = input("Input the password: ")
+        userPasswordUnit = userPasswordName + ":" + userPasswordPassword
+        filePasswords.passwordEntries.append(userPasswordUnit)
+        print("Password has been stored")
+        pass
+    elif x == 3:
+        print("Passwords:")
+        for oj in filePasswords.passwordEntries:
+            print(oj)
+    elif x == 4:
+        programUpkeep = False
+        filePasswords.endingProgram("Passwords.csv")
+        print("Exiting the program.")
+    else:
+        print("Invalid choice. Please try again.")
--- PasswordReader.py
+++ PasswordReader.py
@@ -9,12 +9,29 @@ class PasswordReader:
         try:
             with open(filename, 'r') as file:
                 csv_reader = csv.reader(file, delimiter=':')
-                next(csv_reader)
-                for row in csv_reader:
-                    if len(row) >= 2:
-                        unit, password = row[:2]
-                        self.passwordEntries.append((unit.strip(), password.strip()))
+                # Check if there is data available before calling next()
+                first_row = next(csv_reader, None)
+                if first_row is not None:
+                    for row in csv_reader:
+                        if len(row) >= 2:
+                            # Join the elements of the row using the delimiter ':'
+                            # and append the resulting string to the list
+                            row_str = ':'.join(row)
+                            self.passwordEntries.append(row_str)
         except FileNotFoundError:
             print("File not found :(")
-    
+
+    def endingProgram(self, filename):
+        try:
+            with open(filename, mode='w', newline='') as file:
+                csv_writer = csv.writer(file)  # Specify the delimiter as ':'
+                csv_writer.writerow(["Unit:Password"])
+                for row in self.passwordEntries:
+                    csv_writer.writerow([row])
+        except FileNotFoundError:
+            print("File not found :(")
+
+# Rest of your code (not shown in this example)
+
+
         
